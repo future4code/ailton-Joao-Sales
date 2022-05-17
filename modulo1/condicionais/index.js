677c0b673ac8912ecbbcffdd7b5edf09e2e3a345
Gabriel Sales <joaogabrsales@gmail.com>
2022-05-17 20:36:43 -0300

    exercicio condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,145 @@
+//EXERCICIOS DE CONDICIONAIS
+
+//INTERPRETAÇÃO DE CODIGOS
+
+//QUESTÃO 01
+//A) UMA CONDICIONAL COM UMA PARA DESCOBRIR SE O NUMERO É IMPAR OU PAR.
+
+//B)NUMEROS PARES.
+
+//C)NUMEROS IMPARES.
+
+//QUESTÃO 02 
+
+//A)PARA INDICAR O VALOR DA FRUTA QUE O USUARIO ESCOLHER.
+
+//B)O preço da fruta  Maçã  é  R$  2.25.
+
+//C)O preço da fruta  Pêra  é  R$  5.
+
+//QUESTÃO 03
+
+//A)pedindo um numero ao usuario.
+
+//B)Esse número passou no teste. caso fosse -10 não retornaria nada.
+
+//C)mensagem is not defined, esse erro foi retornado pois a variavél mensagem
+//esta dentro do escopo da condicional, e não no escopo global.
+
+//EXCERCICIOS DE INTERPRETAÇÃO DE CODIGO
+
+//QUESTÃO 01
+
+// let canDrive = Number(prompt('digite sua idade'))
+
+// if (canDrive >= 18) {
+//     console.log('você pode dirigir')
+// } else {
+//     console.log('você não pode dirigir')
+// }
+
+//QUESTÃO 02
+
+// let turnoEstudo = prompt(`Digite M se você estuda no periodo matutino
+//            V se estuda no periodo vespertino 
+//            N se estuda no periodo noturno`)
+
+// if (turnoEstudo === 'M') {
+//     console.log('Bom dia!')
+// } else if (turnoEstudo === 'V') {
+//     console.log('Boa tarde!')
+// } else if (turnoEstudo === 'N') {
+//     console.log('Boa noite!')
+// }
+
+//QUESTÃO 03
+
+
+// let turnoEstudo2 = prompt(`Digite M se você estuda no periodo matutino
+//            V se estuda no periodo vespertino 
+//            N se estuda no periodo noturno`)
+
+// const retornaHorario = (hora) => {
+//     switch (hora) {
+//         case 'M':
+//             return `Bom dia!`
+//         case 'V':
+//             return `Boa tarde!`
+//         case 'N':
+//             return `Boa Noite!`
+//         default:
+//             return `Periodo incorreto`
+//     }
+// }
+
+// console.log(retornaHorario(turnoEstudo2))
+
+//QUESTÃO 04
+
+//fantasia
+//>15
+
+// const generoFilme = prompt('Qual o genero de filme iremos assistir?')
+// const valorIngresso = Number(prompt('Digite o valor do ingresso'))
+
+// if (generoFilme === 'fantasia' && valorIngresso < 15) {
+//     console.log('Bom filme!')
+// } else {
+//     console.log('Escolha outro filme :(')
+// }
+
+//DESAFIOS
+
+//DESAFIO 01
+
+// const generoFilme = prompt('Qual o genero de filme iremos assistir?')
+// const valorIngresso = Number(prompt('Digite o valor do ingresso'))
+// const snack = prompt('Qual snack você quer comprar?')
+
+// if (generoFilme === 'fantasia' && valorIngresso < 15) {
+//     console.log('Bom filme!')
+//     console.log(`Aproveite seu ${snack}`)
+// } else {
+//     console.log('Escolha outro filme :(')
+// }
+
+//DESAFIO 02
+
+// const nomeCompleto = prompt('Digite seu nome completo')
+// const tipoDeJogo = prompt(`                 Digite o tipo de jogo:
+//                     IN - internacional
+//                     DO - doméstico`)
+// const categorias = Number(prompt('digite a categoria: 1, 2, 3 ou 4'))
+// const quantidadeIngressos = Number(prompt('Qauntos ingressos você quer comprar?'))
+
+constQuestions = () => {
+    nome = prompt('Digite seu nome completo'),
+        tipoDeJogo = prompt(`Digite o tipo de jogo: IN - internacional / DO - doméstico`),
+        etapaDeJogo = prompt('idique a etapa do jogo: SF - DT - FI'),
+    categorias = Number(prompt('digite a categoria: 1, 2, 3 ou 4')),
+        quantidadeIngressos = Number(prompt('Qauntos ingressos você quer comprar?'))
+        
+}
+console.log(`
+            ---Dados da compra---
+            Nome do cliente: $ {}
+            Tipo de jogo: $ {}
+            Etapa do jogo: $ {}
+            categoria: $ {}
+            Quantidade de ingressos $ {}
+            ---valores---
+            Valor do ingresso: $ {}
+            Valor total: $ {}`)
+
+retornaDados = (dados) => {
+    switch (dados) {
+        case : 'SF'
+        return 'semi-finais'
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+     
