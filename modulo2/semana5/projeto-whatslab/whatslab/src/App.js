ca9167930c2ecf6ccba66b71fcad06cf3cc51ec9
Gabriel Sales <joaogabrsales@gmail.com>
2022-06-11 03:24:38 -0300

    projeto whatslab layout

--- modulo2/semana5/projeto-whatslab/whatslab/src/App.js
+++ modulo2/semana5/projeto-whatslab/whatslab/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+// import SecaoBody from './components/SecaoBody/SecaoBody';
+
+const CentralContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%; 
+  background-color: black;
+`
+const HeadContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  border-top: 1px solid black;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
+  height: 50px;
+  background-color: orange;
+  color: white;
+`
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  border: 1px solid black;
+  height: 500px;
+  background-color: gray;
+  justify-content: flex-end;
+  align-items: flex-end;
+`
+const MensageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  border-bottom: 1px solid black;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
+  height: 30px;
+  background-color: orange;
+`
+const TesteP = styled.p`
+  flex-direction: column;
+  color: white;
+  box-shadow: 1.5px 1.8px darkgray;
+  padding: 2px 10px;
+  background-color:darkorange;
+  border-radius: 10px;
+  margin: 5px;
+`
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CentralContainer >
+     <HeadContainer>
+      <h2>whats lab</h2>
+     </HeadContainer>
+     <MainContainer>
+    <TesteP>teste</TesteP>
+    <TesteP>teste dois</TesteP>
+    <TesteP>teste tres</TesteP>
+    <TesteP>o rato roeu a roupa do rei de roma</TesteP>
+     </MainContainer>
+     <MensageContainer>
+
+     </MensageContainer>
+    </CentralContainer>
   );
 }
 
