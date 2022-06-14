45a7e03e8c1e3dbdc99b766abee12b4f926c1dd3
Gabriel Sales <joaogabrsales@gmail.com>
2022-06-13 23:15:56 -0300

    exercicio renderização condicional2

--- modulo2/semana5/rend-condicional-form/lista-formulario/src/App.js
+++ modulo2/semana5/rend-condicional-form/lista-formulario/src/App.js
@@ -3,6 +3,16 @@ import Etapa1 from './components/Etapa1'
 import Etapa2 from './components/Etapa2'
 import Etapa3 from './components/Etapa3'
 import EtapaFinal from './components/EtapaFinal'
+import styled from 'styled-components'
+
+const DivPai = styled.div `
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+color:blue;
+background-color: gray;
+`
 
 export default class App extends Component {
   state = {
@@ -30,11 +40,12 @@ export default class App extends Component {
 
     render() {
       return (
-        <div>
+        <DivPai>
           {this.renderScreen()}
+
           {this.state.tela === 4 || <button onClick={this.pulaEtapa}>Proxima Etapa</button> }
           
-        </div>
+        </DivPai>
       )
     }
   }
