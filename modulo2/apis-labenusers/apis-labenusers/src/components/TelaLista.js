e7acc2f4637972c9d57d936268e9eeed422e3439
Gabriel Sales <joaogabrsales@gmail.com>
2022-06-23 00:50:55 -0300

    commit api quase final

--- modulo2/apis-labenusers/apis-labenusers/src/components/TelaLista.js
+++ modulo2/apis-labenusers/apis-labenusers/src/components/TelaLista.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
-import { Containerheader, ContainerPai, ContainerMain, ContainerQuadradinScroll ,  InButton, DivLado  } from '../styled'
+import {  ContainerPai, ContainerMain, ContainerQuadradinScroll, InButton, DivLado, InButtonCinza } from '../styled'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const Espacamento = styled.div`
+width: 420px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 10px;
+gap:10px;
+box-shadow: 2px 3px 2px 3px grey;
+border-radius: 8px;
+margin: 16px;
+`
+
 
 export default class TelaLista extends Component {
     state = {
-        usuarios:[]
+        usuarios: []
     }
 
     componentDidMount() {
@@ -13,56 +27,53 @@ export default class TelaLista extends Component {
 
 
 
-    getAllUsers = () => {
-        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-            {
+    getAllUsers = async () => {
+        try {
+            const response = await axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", {
                 headers: {
-                    Authorization: "joao-sales-ailton"
+                    Authorization: 'joao-sales-ailton'
                 }
-            }
-        )
-            .then((response) => {
-                console.log(response.data)
-                this.setState({ usuarios: response.data })
             })
-            .catch((error) => {
-                console.log(error.message)
-            })
+            console.log('sdcbj', response.data)
+            this.setState({ usuarios: response.data })
+        } catch (error) {
+            console.log(error.response)
+        }
     }
 
     deleteUser = (id) => {
-        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
-        {
-            headers: {
-                Authorization: "joao-sales-ailton"
-            }
+        if (window.confirm("você deseja apagar esse usuario?")) {
+            axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+                {
+                    headers: {
+                        Authorization: "joao-sales-ailton"
+                    }
+                })
+            const arrayNovo = this.state.usuarios.filter((item) => {
+                return item.id !== id
+            })
+            this.setState({ usuarios: arrayNovo })
         }
-        ) 
-        const arrayNovo = this.state.usuarios.filter((item) => {
-            return item.id !== id
-    })
-    this.setState({usuarios: arrayNovo})
     }
 
-
-
+    
     render() {
 
 
         return (
             <ContainerPai>
-            
+
                 <ContainerMain>
                     <ContainerQuadradinScroll>
                         <div>
                             {this.state.usuarios?.map((user) => {
-                               return <div key={user.id}>{user.name}
-                               <InButton onClick={() => this.deleteUser(user.id)}>Vai de base</InButton>
-                               </div>
+                                return <Espacamento key={user.id}> <strong>{user.name}</strong>
+                                    <InButtonCinza onClick={() => this.deleteUser(user.id)}>Vai de base</InButtonCinza>
+                                </Espacamento>
                             })}
                         </div>
                         <DivLado>
-                        <InButton onClick={this.props.telaInicio}>Criar Usuario</InButton>
+                            <InButton onClick={this.props.telaInicio}>Criar Usuario</InButton>
                         </DivLado>
                     </ContainerQuadradinScroll>
                 </ContainerMain>
--- modulo2/apis-labenusers/apis-labenusers/src/styled.js
+++ modulo2/apis-labenusers/apis-labenusers/src/styled.js
@@ -8,20 +8,12 @@ height: 100%;
 width: 100%;
 `
 
-export const Containerheader = styled.header`
-height: 20%;
-border: 1px solid black;
-display: flex;
-justify-content: center;
-align-items: center;
-`
-
 export const ContainerMain = styled.div`
 min-height: 100vh;
 display: flex;
 align-items: center;
 justify-content: center;
-background-color: blanchedalmond;
+background-color: black;
 `
 
 export const ContainerQuadradin = styled.div`
@@ -33,38 +25,41 @@ width: 500px;
 height: 500px;
 background-color: darkorange;
 border-radius: 24px;
-box-shadow: 12px 16px 12px 16px grey;
+box-shadow: 6px 6px 24px 32px grey;
 `
 export const ContainerQuadradinScroll = styled.div`
 display: flex;
 align-items: center;
-justify-content: center;
+justify-content: space-between;
 flex-direction: column;
 width: 500px;
 height: 500px;
 overflow: auto;
 border-radius: 24px;
-box-shadow: 12px 16px 12px 16px grey;
+box-shadow: 6px 6px 24px 32px grey;
 background-color: darkorange;
 `
 
-export const ContainerFooter = styled.footer`
-height: 20%;
-border: 1px solid black;
-display: flex;
-justify-content: center;
-align-items: center;
-`
-export const Inputo =styled.input`
+
+export const Inputo = styled.input`
 border-radius: 8px;
 border: none;
 padding: 10px;
 `
-export const InButton =styled.button`
+export const InButton = styled.button`
+border-radius: 8px;
+border: none;
+margin-top: 10px;
+margin-bottom: 10px;
+padding: 10px;
+`
+export const InButtonCinza = styled.button`
 border-radius: 8px;
 border: none;
 margin-top: 10px;
 padding: 10px;
+background-color: gray;
+color: white;
 `
 export const DivLado = styled.div`
 margin-left: 20px;
