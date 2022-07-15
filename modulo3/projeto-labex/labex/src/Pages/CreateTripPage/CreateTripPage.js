b7561d5f47e0b8361675fc3ea1680f65dc07d6d5
Gabriel Sales <joaogabrsales@gmail.com>
2022-07-15 20:47:53 -0300

    metade da função de createTrip implementada

--- modulo3/projeto-labex/labex/src/Pages/CreateTripPage/CreateTripPage.js
+++ modulo3/projeto-labex/labex/src/Pages/CreateTripPage/CreateTripPage.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import {goPage} from '../routes/Coordinator'
+import { Base_URL } from '../../Constants/Base_URL'
+import { goPage } from '../routes/Coordinator'
+import { DivForm } from './Styled'
 
 export const CreateTripPage = () => {
   const navigate = useNavigate()
+  const [name, setName] = useState('')
+  const [planet, setPlanet] = useState([])
+  const [date, setDate] = useState('')
+  const [description, setDescription] = useState('')
+  const [days, setDays] = useState('')
+
+  const createTrip = async () => {
+    console.log('body', body)
+    const token = localStorage.getItem('token')
+    const body = {
+      name: name,
+      planet: planet,
+      date: date,
+      description: description,
+      durationInDays: days
+    }
+    try {
+      const res = await axios.post(Base_URL + `trips/`, body, {
+        headers: {
+          auth: token
+        }
+      })
+      console.log('viagem criada', res.data)
+    } catch (err) {
+      console.log('deu ruim', err.response)
+    }
+  }
 
   return (
-    <div>CreateTripPage</div>
+    <div>
+      <h1>CreateTripPage</h1>
+      <div>
+        <DivForm>
+          <input
+            placeholder={'Nome'}
+            value={name}
+          />
+          <select>
+            <option>Teste</option>
+          </select>
+          <input
+            placeholder={'Data'}
+            value={date}
+          />
+          <input
+            placeholder={'Descrição'}
+            value={description}
+          />
+          <input
+            placeholder={'Duração da viagem'}
+            value={days}
+          />
+          <button>Criar viagem</button>
+        </DivForm>
+        <button>voltar</button>
+      </div>
+    </div>
   )
 }
---
+++ modulo3/projeto-labex/labex/src/Pages/CreateTripPage/Styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const DivForm = styled.form`
+display: flex;
+flex-direction: column; 
+justify-content: center;
+width: 400px;
+`
