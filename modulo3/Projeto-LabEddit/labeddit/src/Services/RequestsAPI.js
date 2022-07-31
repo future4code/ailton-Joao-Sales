5446f3c8338940bd20a69a96971d8aa48c26825d
Gabriel Sales <joaogabrsales@gmail.com>
2022-07-31 12:10:47 -0300

    add changepost and changecomment requests

--- modulo3/Projeto-LabEddit/labeddit/src/Services/RequestsAPI.js
+++ modulo3/Projeto-LabEddit/labeddit/src/Services/RequestsAPI.js
@@ -42,9 +42,21 @@ export const GetPosts = async (setPost) => {
     }
 }
 
-// export const GetPostComments = async () => {
+// falta aplicar a função de buscar comentarios
 
-// }
+export const GetPostComments = async (id) => {
+    const token = localStorage.getItem('token')
+    try {
+        const res = await axios.get(BaseURL + `posts/${id}/comments`, {
+            headers: {
+                authorization: token
+            }
+        })
+        console.log('comentario', res.data)
+    } catch (err) {
+        console.log('erro comentario', err.response)
+    }
+}
 
 export const CreatePost = async (form) => {
     const token = localStorage.getItem('token')
@@ -60,4 +72,44 @@ export const CreatePost = async (form) => {
     }
 }
 
+export const CreateComment = async (id, form) => {
+    const token = localStorage.getItem('token')
+    try {
+        const res = await axios.post(BaseURL + `posts/${id}/comments`, form, {
+            headers: {
+                authorization: token
+            }
+        })
+        console.log('comentario criado', res.data)
+    } catch (err) {
+        console.log('erro ao criar comentario', err.response)
+    }
+}
 
+export const ChangePostVote = (id) => {
+    const token = localStorage.getItem('token')
+    try {
+        const res = axios.put(BaseURL + `posts/${id}/votes`, {
+            headers: {
+                authorization: token
+            }
+        })
+        console.log('changeCvote', res.data)
+    } catch (err) {
+        console.log('erro ao mudar voto', err.response)
+    }
+}
+
+export const ChangeCommentVote = (id) => {
+    const token = localStorage.getItem('token')
+    try {
+        const res = axios.put(BaseURL + `comments/${id}/votes`, {
+            headers: {
+                authorization: token
+            }
+        })
+        console.log('changeCvote', res.data)
+    } catch (err) {
+
+    }
+}
