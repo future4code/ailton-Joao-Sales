2f1189000e13076badd581be9a971f68088ebf28
Gabriel Sales <joaogabrsales@gmail.com>
2022-08-17 21:00:12 -0300

    fluxo de dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,33 @@
+export type product = {
+  id: string;
+  name: string;
+  price: number;
+};
+
+export const products: product[] = [
+  {
+    id: "1",
+    name: "notebook",
+    price: 500,
+  },
+  {
+    id: "2",
+    name: "computer",
+    price: 550,
+  },
+  {
+    id: "3",
+    name: "camera",
+    price: 400,
+  },
+  {
+    id: "4",
+    name: "car",
+    price: 5000,
+  },
+  {
+    id: "5",
+    name: "bike",
+    price: 200,
+  },
+];
