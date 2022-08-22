0d73507f2f676851cbd3485926af3741cbd76b6e
Gabriel Sales <joaogabrsales@gmail.com>
2022-08-21 21:05:35 -0300

    requisitos minimos feitos

---
+++ modulo4/sistema-bancario/src/accounts.ts
@@ -0,0 +1,39 @@
+import { Account } from "./types";
+
+export const accounts: Account[] = [
+  {
+    name: "João",
+    CPF: "123.456.789-00",
+    dateOfBirth: new Date("1990-01-01"),
+    balance: 0,
+    statement: [],
+  },
+  {
+    name: "Maria",
+    CPF: "123.456.789-00",
+    dateOfBirth: new Date("1990-01-01"),
+    balance: 0,
+    statement: [],
+  },
+  {
+    name: "Pedro",
+    CPF: "123.456.789-00",
+    dateOfBirth: new Date("1990-01-01"),
+    balance: 0,
+    statement: [],
+  },
+  {
+    name: "Joana",
+    CPF: "123.456.789-00",
+    dateOfBirth: new Date("1990-01-01"),
+    balance: 0,
+    statement: [],
+  },
+  {
+    name: "José",
+    CPF: "123.456.789-00",
+    dateOfBirth: new Date("1990-01-01"),
+    balance: 0,
+    statement: [],
+  },
+];
